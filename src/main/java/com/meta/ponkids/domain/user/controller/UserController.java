4cf7966781db8ea2e53f02a3fd6b0683c33eee07
jjeovi@gmail.com <jjeovi@gmail.com>
2023-11-17 23:24:11 +0900

    개발 현재까지 commit

--- src/main/java/com/meta/ponkids/domain/user/controller/UserController.java
+++ src/main/java/com/meta/ponkids/domain/user/controller/UserController.java
@@ -1,33 +1,20 @@
 package com.meta.ponkids.domain.user.controller;
 
-import java.time.LocalDateTime;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.transaction.Transactional;
-
+import com.meta.ponkids.domain.system.role.repository.RoleRepository;
+import com.meta.ponkids.domain.user.dto.*;
+import com.meta.ponkids.domain.user.repository.UserRepository;
+import com.meta.ponkids.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import com.meta.ponkids.domain.system.role.repository.RoleRepository;
-import com.meta.ponkids.domain.user.dto.MultiUserChldrnSaveReqDto;
-import com.meta.ponkids.domain.user.dto.UserListDto;
-import com.meta.ponkids.domain.user.dto.UserRoleSaveReqDto;
-import com.meta.ponkids.domain.user.dto.UserSaveReqDto;
-import com.meta.ponkids.domain.user.repository.UserRepository;
-import com.meta.ponkids.domain.user.service.UserService;
-
-import lombok.RequiredArgsConstructor;
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,14 +27,14 @@ public class UserController {
     private final static String BASIC_PATH = "/admin/user";
     
     /**
-     * methodName    : userList
-     * date           : 10/28/23
-     * description    :
+     * methodName    : list
+     * date           : 11/17/23
+     * description    : user list method
      */
     @GetMapping( BASIC_PATH + "/list" )
-    public String list( @ModelAttribute UserListDto userListDto, 
-    					@PageableDefault( size = 10 ) Pageable pageable,
-    					Model model ) {
+    public String list( @ModelAttribute UserListDto userListDto,
+                        @PageableDefault( size = 10 ) Pageable pageable,
+                        Model model ) {
         
         // 목록 조회
         Page<UserListDto> resultList = userService.getList( userListDto, pageable );
@@ -57,12 +44,16 @@ public class UserController {
         model.addAttribute( "searchDTO", userListDto );
         
         // 기본 경로 setting
-        model.addAttribute("basicPath", BASIC_PATH);
-
+        model.addAttribute( "basicPath", BASIC_PATH );
+        
         return BASIC_PATH + "/list";
     }
     
-    
+    /**
+     * methodName    : regist
+     * date           : 11/17/23
+     * description    : user regist method
+     */
     @GetMapping( BASIC_PATH + "/regist" )
     public String regist( Model model ) {
         
@@ -73,12 +64,17 @@ public class UserController {
         model.addAttribute( new UserSaveReqDto() );
         
         // 기본 경로 setting
-        model.addAttribute("basicPath", BASIC_PATH);
+        model.addAttribute( "basicPath", BASIC_PATH );
         
         return BASIC_PATH + "/regist";
     }
     
     
+    /**
+     * methodName    : insert
+     * date           : 11/17/23
+     * description    : user insert method
+     */
     @PostMapping( BASIC_PATH + "/insert" )
     public String insert(
             @ModelAttribute UserSaveReqDto userSaveReqDto,
@@ -92,7 +88,7 @@ public class UserController {
             
             // 메시지 출력 및 url 이동 처리
             model.addAttribute( "resultMsg", "해당ID로 가입된 ID가 있습니다. 다시 시도해주세요." );
-            model.addAttribute( "moveUrl", BASIC_PATH +"/list" );
+            model.addAttribute( "moveUrl", BASIC_PATH + "/list" );
             
             return "common/alert";
             
@@ -111,78 +107,95 @@ public class UserController {
         
         // 메시지 출력 및 url 이동 처리
         model.addAttribute( "resultMsg", "정상적으로 등록되었습니다." );
-        model.addAttribute( "moveUrl", BASIC_PATH +"/list" );
+        model.addAttribute( "moveUrl", BASIC_PATH + "/list" );
         
         return "common/alert";
     }
     
-    
-    @GetMapping(value= { BASIC_PATH + "/detail", 
-    		             BASIC_PATH + "/modify" } )	 
+    /**
+     * methodName    : detailOrModify
+     * date           : 11/17/23
+     * description    : user detail or user modify method
+     */
+    @GetMapping( value = { BASIC_PATH + "/detail",
+            BASIC_PATH + "/modify" } )
     public String detailOrModify(
-            @RequestParam(required = true) String userId,
+            @RequestParam( required = true ) String userId,
             Model model,
             HttpServletRequest request ) {
-    	
-    	// 권한 리스트
+        
+        // 권한 리스트
         model.addAttribute( "authList", roleRepository.findAll() );
         
         // target object 조회
-        model.addAttribute("targetDto", userService.findByUserId(userId));
+        model.addAttribute( "targetDto", userService.findByUserId( userId ) );
         
         // 기본 경로 setting
-        model.addAttribute("basicPath", BASIC_PATH);
+        model.addAttribute( "basicPath", BASIC_PATH );
         
         
         String urlPath = request.getServletPath();
-        String remainPath = ""; 
-        if ( urlPath.split(BASIC_PATH)[1].startsWith("/detail") ) remainPath = "detail";
-        if ( urlPath.split(BASIC_PATH)[1].startsWith("/modify") ) remainPath = "modify";
+        String remainPath = "";
+        if ( urlPath.split( BASIC_PATH )[ 1 ].startsWith( "/detail" ) ) remainPath = "detail";
+        if ( urlPath.split( BASIC_PATH )[ 1 ].startsWith( "/modify" ) ) remainPath = "modify";
         
         
         return BASIC_PATH + "/" + remainPath;
     }
     
-    
-    @PostMapping(BASIC_PATH + "/update")
+    /**
+     * methodName    : update
+     * date           : 11/17/23
+     * description    : user update method
+     */
+    @PostMapping( BASIC_PATH + "/update" )
     public String update(
-    		@RequestParam(required = true) String userId,
-    		Model model ) {
-    	
-    	// 권한 리스트
-    	model.addAttribute( "authList", roleRepository.findAll() );
-    	model.addAttribute("modDto", userService.findByUserId(userId));
-    	
-    	// 기본 경로 setting
-    	model.addAttribute("basicPath", BASIC_PATH);
-    	
-    	return BASIC_PATH + "/update";
-    }
-    
-    
-    @Transactional
-    @PostMapping( BASIC_PATH + "/delete")
-    public String delete(
-            @RequestParam(required = true) String userId,
+            @RequestParam( required = true ) String userId,
+            @ModelAttribute UserModDto modDto,
             Model model ) {
         
-    	// 삭제 처리 
-        userService.deleteAllByUserId( userId );	
+        // TODO update 구현
         
         // 메시지 출력 및 url 이동 처리
-        model.addAttribute( "resultMsg", "정상적으로 삭제되었습니다." );
-        model.addAttribute( "moveUrl", BASIC_PATH +"/list" );
+        model.addAttribute( "resultMsg", "정상적으로 등록되었습니다." );
+        model.addAttribute( "moveUrl", BASIC_PATH + "/list" );
         
         return "common/alert";
     }
     
-    @ResponseBody
-    @RequestMapping( value = "/live/idDupCheck", method = { RequestMethod.GET } )
-    public boolean ipDupCheck( @RequestParam( "userId" ) String userId ) {
-    	
-        return userRepository.existsByUserId( userId );
+    
+    /**
+     * methodName    : delete
+     * date           : 11/17/23
+     * description    : user delete method
+     */
+    @Transactional
+    @PostMapping( BASIC_PATH + "/delete" )
+    public String delete(
+            @RequestParam( required = true ) String userId,
+            Model model ) {
+        
+        // 삭제 처리
+        userService.deleteAllByUserId( userId );
+        
+        // 메시지 출력 및 url 이동 처리
+        model.addAttribute( "resultMsg", "정상적으로 삭제되었습니다." );
+        model.addAttribute( "moveUrl", BASIC_PATH + "/list" );
+        
+        return "common/alert";
     }
     
+    /**
+     * methodName    : idDupCheck
+     * date           : 11/17/23
+     * description    : id 중복체크 ajax
+     */
+    @ResponseBody
+    @RequestMapping( value = "/live/idDupCheck", method = { RequestMethod.GET } )
+    public boolean idDupCheck( @RequestParam( "userId" ) String userId ) {
+        
+        return userRepository.existsByUserId( userId );
+    }
     
     
 }
