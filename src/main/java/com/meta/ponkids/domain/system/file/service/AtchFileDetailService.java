3a9c74bcc1ed49d6be81c851be0f2e9078b94e8c
USER <USER@DESKTOP-M9OM3GE>
2023-12-07 17:53:43 +0900

    첨부파일 다운로드 작업중

--- src/main/java/com/meta/ponkids/domain/system/file/service/AtchFileDetailService.java
+++ src/main/java/com/meta/ponkids/domain/system/file/service/AtchFileDetailService.java
@@ -1,7 +1,13 @@
 package com.meta.ponkids.domain.system.file.service;
 
+import com.meta.ponkids.domain.system.file.entity.AtchFileDetail;
 import com.meta.ponkids.domain.system.file.repository.AtchFileDetailRepository;
+import com.meta.ponkids.domain.system.ntt.dto.NttReplyListDto;
+
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +15,11 @@ import org.springframework.stereotype.Service;
 public class AtchFileDetailService {
     
     private final AtchFileDetailRepository atchFileDetailRepository;
+    
+    // 파일 다건 목록 조회
+	public List<AtchFileDetail> getList(Long atchFileSn) {
+
+		return atchFileDetailRepository.getList(atchFileSn);
+	}
+	
 }
