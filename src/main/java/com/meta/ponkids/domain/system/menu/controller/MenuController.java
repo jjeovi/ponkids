545186846426863e2e1ccf508cbe231d55cf1d97
USER <USER@DESKTOP-M9OM3GE>
2024-01-23 18:02:30 +0900

    commit

---
+++ src/main/java/com/meta/ponkids/domain/system/menu/controller/MenuController.java
@@ -0,0 +1,53 @@
+package com.meta.ponkids.domain.system.menu.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.meta.ponkids.domain.system.menu.dto.MenuListDto;
+import com.meta.ponkids.domain.system.menu.service.MenuService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MenuController {
+    
+    private final static String BASIC_PATH = "/menu";
+    private final MenuService menuService;
+    @Value( "${key.default.admin}" )
+    private String TYPE_ADMIN;
+    
+    @Value( "${key.default.user}" )
+    private String TYPE_USER;
+    
+    @Value( "${key.default.adminRootMenuSn}" )
+    private Long ADMIN_ROOT_MENU_SN;
+    
+    @Value( "${key.default.userRootMenuSn}" )
+    private Long USER_ROOT_MENU_SN;
+    
+    @ResponseBody
+    @GetMapping( BASIC_PATH + "/live/{type}/getMenuListAjax" )
+    public Map<String, Object> getMenuListAjax( @ModelAttribute MenuListDto listDto,
+                                                @PathVariable String type ) {
+        // 해당 권한에 맞는 menuList 가져온 뒤 drawMenuTree 로 메뉴를 그린다.
+        Map<String, Object> result = new HashMap<String, Object>();
+        
+        // 메뉴 list 출력
+        if ( type.equals( TYPE_USER ) ) {
+            result.put( "resultList", menuService.getUserMenuList( listDto ) );
+            
+        }
+        
+        return result;
+    }
+    
+    
+}
--- src/main/resources/static/pon/common/js/common.js
+++ src/main/resources/static/pon/common/js/common.js
@@ -1,6 +1,43 @@
 
 // ------------- function () 함수 실행  호출시점 : DOM Tree 생성 완료 후 -----------------
 $( function () {
+	
+	// pon menu list set
+	var url = '/menu/live/pon/getMenuListAjax';
+	$.ajax( {
+                url: url,
+                type: "GET",
+                dataType: "json",
+                async: false,	// 동기식 ajax : 통신이 완료될 떄 까지 다음 line 진행 안함
+                data: cateSchData, // 검색할 값
+                contentType: "application/json",
+                success: function ( result ) {
+					
+                    // return type : List<MenuListDto>
+                    var allYn = $ul.data( 'allYn' );
+                    var nextStepLiOnclickParamUrl = $ul.data( 'nextStepLiOnclickParamUrl' );
+
+                    if ( nextStepLiOnclickParamUrl != null && nextStepLiOnclickParamUrl != '' ) {
+                        url = nextStepLiOnclickParamUrl;
+                    } else {
+                        url = '';
+                    }
+
+                    if ( allYn != null && allYn == 'Y' ) {
+                        $( ".category-list-area .category-group-box ul" ).eq( ulNum + 1 ).append(
+                            $( "<li>" ).attr( "onclick", "getCateNextLvList('" + url + "', this )" ).attr( "value", "" ).append( "전체" )
+                        );
+                    }
+                    if( result.resultList != null && result.resultList.length > 0 ) {
+		                for ( let item of result.resultList ) {
+		                    $( ".category-list-area .category-group-box ul" ).eq( ulNum + 1 ).append(
+		                        $( "<li>" ).attr( "onclick", "getCateNextLvList('" + url + "', this )" ).attr( "value", item.category['categorySn'] ).append( item.category['categoryNm'] )
+		                    );
+		                }
+                    }
+                }
+            } );
+	
 
 
 
