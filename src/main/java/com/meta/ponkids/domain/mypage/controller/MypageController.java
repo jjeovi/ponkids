9bbacac4a49b7feee38c0b50699ed285d552dd87
USER <USER@DESKTOP-M9OM3GE>
2024-02-27 19:06:28 +0900

    commit

--- src/main/java/com/meta/ponkids/domain/mypage/controller/MypageController.java
+++ src/main/java/com/meta/ponkids/domain/mypage/controller/MypageController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
 
@@ -61,7 +60,6 @@ public class MypageController {
 		model.addAttribute( "basicPath", BASIC_PATH );
 		return USER_VIEW_PATH + BASIC_PATH + "/myInfoModify";
 	}
-	
 	
 	@GetMapping( BASIC_PATH + "/childInfoModify" )
 	public String childInfoModify( Model model ) {
